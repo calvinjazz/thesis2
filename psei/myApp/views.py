05834a6b93b16a55691509d8f50c81a3519c5632
calvinjazz <calvinjazz.0330@gmail.com>
2021-09-17 04:36:59 +0800

    integrate k-nn into django framework

--- psei/myApp/views.py
+++ psei/myApp/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from pandas.core.frame import DataFrame
 from .models import Feature
+
+import pandas as pd
+import numpy as np
+from sklearn import metrics
+import matplotlib.pyplot as plt
+from io import StringIO
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import confusion_matrix, accuracy_score
+import math
 
 # Create your views here.
 def index(request):
@@ -59,3 +70,80 @@ def counter(request):
 
 def post(request, pk):
     return render(request, 'post.html', {'pk': pk})
+
+def chart(request):
+    dataset = pd.read_csv('test-dataset.csv')
+    dataset['date'] = pd.to_datetime(dataset.timestamp)
+    datasetHead = dataset.head()
+    datasetDescribe = dataset.describe()
+
+    #knn regression
+    X  = dataset[['open','high','low','volume']]
+    y = dataset['close']
+
+    #separate training and testing
+    X_train , X_test , y_train , y_test = train_test_split(X ,y , random_state = 0)
+        
+    #train
+    regressor = LinearRegression()
+    regressor.fit(X_train,y_train)
+
+    coef = regressor.coef_
+
+    predicted=regressor.predict(X_test)
+
+    dframe=pd.DataFrame(y_test,predicted)
+    dfr=pd.DataFrame({'Actual':y_test,'Predicted':predicted})
+
+    #check accuracy
+    score = regressor.score(X_test,y_test)
+    mae = metrics.mean_absolute_error(y_test,predicted)
+    mse = metrics.mean_squared_error(y_test,predicted)
+    rmse = math.sqrt(metrics.mean_squared_error(y_test,predicted))
+
+    if request.method == 'POST':
+        userOpen = float(request.POST['userOpen'])
+        userHigh = float(request.POST['userHigh'])
+        userLow = float(request.POST['userLow'])
+        userVolume = int(request.POST['userVolume'])
+        userParameters = [[userOpen, userHigh, userLow, userVolume]]
+        predictedValue = regressor.predict(userParameters)
+
+    else:
+        userOpen = ''
+        userHigh = ''
+        userLow = ''
+        userVolume = ''
+        predictedValue = ''
+
+    mydick = {
+        'dataset': dataset.to_html(),
+        'datasetHead': datasetHead.to_html(),
+        'datasetDescribe': datasetDescribe.to_html(),
+        'graph': return_graph(dataset),
+        'coef': coef,
+        'xtest': X_test,
+        'dfr': dfr.to_html(),
+        'score': score,
+        'mae': mae,
+        'mse': mse,
+        'rmse': rmse,
+        'userOpen': userOpen,
+        'userHigh': userHigh,
+        'userLow': userLow,
+        'userVolume': userVolume,
+        'predictedValue': predictedValue,
+    }
+    return render(request, 'chart.html', context=mydick)
+
+def return_graph(dataset):
+    fig = plt.figure(figsize=(16,6))
+    plt.plot(dataset['open'])
+
+    imgdata = StringIO()
+    fig.savefig(imgdata, format='svg')
+    imgdata.seek(0)
+
+    data = imgdata.getvalue()
+    
+    return data
