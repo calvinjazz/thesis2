230c7377e4d2033a05e43d43b32fd20091d3d7f7
calvinjazz <calvinjazz.0330@gmail.com>
2022-08-28 21:00:17 +0800

    first commit

--- flutterApp/thesis2_flutter_application/lib/screens/homepage.dart
+++ flutterApp/thesis2_flutter_application/lib/screens/homepage.dart
@@ -10,8 +10,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   openURL() async {
-    if (await canLaunch("https://thesis2-calvinjazz.herokuapp.com/")) {
-      await launch("https://thesis2-calvinjazz.herokuapp.com/",
+    if (await canLaunch("https://thesis2-calvinjazz.herokuapp.com/login")) {
+      await launch("https://thesis2-calvinjazz.herokuapp.com/login",
           forceWebView: true, enableJavaScript: true);
     } else {
       throw 'Could Not Launch URL';
@@ -20,24 +20,6 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              MaterialButton(
-                color: Colors.red,
-                onPressed: () {
-                  openURL();
-                },
-                child: Text("Get started!", style: TextStyle(color: Colors.white)),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+    return openURL();
   }
 }
